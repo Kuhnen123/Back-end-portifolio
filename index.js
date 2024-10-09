662ebd8fff6a8c50ebe7776b076b0e5f2508fc21
Kuhnen123 <marianakuhnenm@gmail.com>
2024-10-09 17:15:25 -0300

    dagvgshsehseheh

--- index.js
+++ index.js
@@ -2,17 +2,50 @@ const express = require('express');
 const cors = require ('cors');
 
 const app = express();
-const porta= 3000;
+const port = 3000;
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/',(req, res)=>{
-    res.status(200).json({message: 'OK'});
-    console.log('oie');
+const vetor = [
+  {mensagem: 'oi', numero: 0},
+  {mensagem: 'eu', numero: 1},
+  {mensagem: 'vou', numero: 2},
+  {mensagem: 'me', numero: 3},
+  {mensagem: 'matar', numero: 4},
+]
+app.post('/falas', (req, res) => {  
+  const mensagem = req.mensagem;
+  console.log(mensagem);
+  res.status(201).json({mensagem: 'sra que Deu boa?'});
 });
 
-app.listen(porta, ()=>{
-    console.log(`Servidor rodando na porta ${porta}`);
+app.delete('/falas ', (req, res)=>{
+    const numero = req.body.numero;
+    vetor.splice(numero,1);
+    console.log(vetor);
+    res.status(201).json({ mensagem: 'deu boa o DELETE'});
 });
 
+app.put('/falas ', (req, res)=>{
+    const numero = req.body.numero;
+    const mensagem = rq.body.mensagem
+    vetor[numero].mensagem - mensagem;
+    console.log(vetor);
+    res.status(201).json({ mensagem: 'deu boa o PUT'});
+});
+
+
+app.get('/falas',(req, res) => {
+    res.status(200).json({vetor});
+     console.log(`não aguento mais o bryam`)
+    });
+
+app.get('/',(req, res) => {
+res.status(200).json({mesagem: 'Olá mundo!!!'});
+ console.log(`Oi`)
+});
+ 
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
+});
